0c21dd388c282d2c44c3c744b76c0c3ba208a7b9
Kaleb Juliu <kalebjuliu@gmail.com>
2020-01-07 14:20:22 +0700

    Added UI

---
+++ DrumPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrumPad
+//
+//  Created by Archangel on 01/01/20.
+//  Copyright © 2020 Kaleb Juliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
